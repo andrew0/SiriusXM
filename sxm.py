f5ae2b8d71a81a0ec536729b90d952028ea5b4f1
fts-tmassey <62904731+fts-tmassey@users.noreply.github.com>
2020-12-09 14:51:11 -0500

    Get credentials from environment variables
    Added ability to use the -e or --env parameter to override the command line username and password with the optional SXM_USER and SXM_PASS environment variables.  This paired with a proper EnvironmentFile in a systemd unit, allows for a secure way of storing your credentials.

--- sxm.py
+++ sxm.py
@@ -5,6 +5,7 @@ import urllib.parse
 import json
 import time, datetime
 import sys
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SiriusXM:
@@ -355,8 +356,14 @@ if __name__ == '__main__':
     parser.add_argument('password')
     parser.add_argument('-l', '--list', required=False, action='store_true', default=False)
     parser.add_argument('-p', '--port', required=False, default=9999, type=int)
+    parser.add_argument('-e', '--env',  required=False, action='store_true', default=False)
     args = vars(parser.parse_args())
-    
+    if args['env']:
+        if "SXM_USER" in os.environ:
+            args['username'] = os.environ.get('SXM_USER')
+        if "SXM_PASS" in os.environ:
+            args['password'] = os.environ.get('SXM_PASS')
+
     sxm = SiriusXM(args['username'], args['password'])
     if args['list']:
         channels = list(sorted(sxm.get_channels(), key=lambda x: (not x.get('isFavorite', False), int(x.get('siriusChannelNumber', 9999)))))
